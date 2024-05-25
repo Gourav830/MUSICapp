13750997da754deb864717874c977d676da8a96c
“[WEb <gouravsingla830@gmail.com>
2024-05-26 04:45:18 +0530

    webinar Section -done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import WhyUs from "@/components/WhyUs";
 import AutoScrollCards from "@/components/AutoScrollCards";
+import Webinar from '../components/webinar';
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
@@ -10,6 +11,7 @@ export default function Home() {
       <FeaturedCourses />
       <WhyUs />
       <AutoScrollCards />
+      <Webinar/>
     </main>
   );
 }
