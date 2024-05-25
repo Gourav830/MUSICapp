edb3eb200c032c260b4392941c1e99e24e1e6930
“[WEb <gouravsingla830@gmail.com>
2024-05-26 02:19:14 +0530

    done with hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import HeroSection from "@/components/HeroSection";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-<h1>Singla ji</h1>      <HeroSection />
+ <HeroSection />
     </main>
   );
 }
