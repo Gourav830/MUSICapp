096ac2b2e312d0e8cfc37847b6cac039c9666486
“[WEb <gouravsingla830@gmail.com>
2024-05-26 20:43:51 +0530

    made contact us

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -4,8 +4,6 @@ import Image from "next/image";
 import React from "react";
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import courseData from "@/data/music_courses.json";
-import Link from "next/link";
-
 interface Course {
   id: number;
   title: string;
@@ -49,23 +47,6 @@ function Page() {
                   alt={course.title}
                 />
               </CardItem>
-              <div className="flex justify-between items-center mt-20">
-                <CardItem
-                  translateZ={20}
-                  as={Link}
-                  href={`/courses/${course.slug}`}
-                  className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                >
-                  Try now →
-                </CardItem>
-                <CardItem
-                  translateZ={20}
-                  as="button"
-                  className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
-                >
-                  Sign up
-                </CardItem>
-              </div>
             </CardBody>
           </CardContainer>
         ))}
